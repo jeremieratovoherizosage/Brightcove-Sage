15996f0f07ba7302a16f460e6bac50453bed325f
Jeremie Ratovoherizo <jeremie.ratovoherizo@sage.com>
2023-03-21 17:44:38 +0100

    Update ListenForParent.js

--- ListenForParent.js
+++ ListenForParent.js
@@ -1,20 +1,25 @@
-videojs.registerPlugin('listenForParent', function() {
-  var myPlayer = this;
-  // This method called when postMessage sends data into the iframe
-  function controlVideo(event){
-    if (event.data === "playerDetail") {
-	console.log('Brightcove: message received from sage.com:  ' + event.data, event);
-	
-	var message = "holla back youngin!";
-	console.log('Brightcove:  sending message to sage.com:  ' + message + " Data: " + JSON.stringify(myPlayer));
-	event.source.postMessage(message, event.origin);
-	} else if(event.data === "playVideo") {
-      myPlayer.play();
-    } else if (event.data === 'pauseVideo') {
-      myPlayer.pause();
-    }
-  };
-  
-  // Listen for the message, then call controlVideo() method when received
-  window.addEventListener("message",controlVideo);
+videojs.registerPlugin('listenForParent', function () {
+    var myPlayer = this;
+    // This method called when postMessage sends data into the iframe
+    function controlVideo(event) {
+        if (event.data === "playerDetail") {
+            console.log('Brightcove: message received from sage.com:  ' + event.data, event);
+
+            var message = "holla back youngin!";
+            console.log('Brightcove:  sending message to sage.com:  ' + message + " Data: " + JSON.stringify(myPlayer));
+            event.source.postMessage(message, event.origin);
+        } else if ("playerCurrentTime") {
+            var currentTime = myPlayer.currentTime();
+            var message = "Video CurrentTime";
+            console.log('Brightcove:  sending message to sage.com:  ' + message + " Data: " + currentTime);
+            event.source.postMessage(message, event.origin);
+        } else if (event.data === "playVideo") {
+            myPlayer.play();
+        } else if (event.data === 'pauseVideo') {
+            myPlayer.pause();
+        }
+    };
+
+    // Listen for the message, then call controlVideo() method when received
+    window.addEventListener("message", controlVideo);
 });
